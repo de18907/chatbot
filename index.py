4581689c9a6cf0316cf4eac396fdf8e895b03b36
Sergio Augusto Minaya Herrera <39938438+de18907@users.noreply.github.com>
2020-08-06 18:00:32 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,44 @@
+from nltk.chat.util import Chat, reflections
+mis_reflexions = {
+"hola": "hola"
+}
+pares = [
+    [
+        r"mi nombre es (.*)",
+        ["Hola %1, como estas?",]
+    ],
+     [
+        r"cual es tu nombre?",
+        ["Mi nombre es Chatbot",]
+    ],
+    [
+        r"como estas ?",
+        ["Bien, y tu?",]
+    ],
+    [
+        r"disculpa (.*)",
+        ["No pasa nada",]
+    ],
+    [
+        r"hola|hey|buenas",
+        ["Hola", "Que tal",]
+    ],
+    [
+        r"que (.*) quieres ?",
+        ["Nada gracias",]
+        
+    ],
+    [
+        r"(.*) creado ?",
+        ["Fui creado hoy",]
+    ],
+    [
+        r"finalizar",
+        ["Chao","Fue bueno hablar contigo"]
+],
+]
+def chatear():
+    print("Hola soy un bot, escribe algo para comenzar") #mensaje por defecto
+    chat = Chat(pares, mis_reflexions)
+    chat.converse()
+chatear()
